80491c00a2752d9185b847d1b4f7f524b791aa42
ismaryanto <kotokareh@gmail.com>
2020-08-09 04:01:01 +0700

    commit data

--- application/libraries/Pdf_tc.php
+++ application/libraries/Pdf_tc.php
@@ -1,4 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	error_reporting(0); 
 require_once APPPATH.'third_party/tcpdf/tcpdf.php';  
 class Pdf_tc extends TCPDF
 {
