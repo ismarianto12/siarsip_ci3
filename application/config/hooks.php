cfa9f764d7350a3ea45a1aaba5b7b9fa669f5e33
ismarianto12 <kotokareh@gmail.com>
2021-05-01 22:47:23 +0700

    update new version pjax s

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['post_controller'][] = array(
+        'class'    => 'Pjax',
+        'function' => 'initialize',
+        'filename' => 'Pjax.php',
+        'filepath' => 'hooks'
+);
