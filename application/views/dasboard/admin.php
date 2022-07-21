0140d10818fc410393e462dfa4709625d4041d22
Isma Rianto <ismarianto97@gmail.com>
2022-07-21 20:54:48 +0700

    adasda

--- application/views/dasboard/admin.php
+++ application/views/dasboard/admin.php
@@ -37,7 +37,7 @@
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="<?= site_url('tbl_surat_keluar') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div><!-- ./col -->
         <div class="col-lg-3 col-xs-6">
